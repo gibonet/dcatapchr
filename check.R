06ebdafe8c967f92e39026d11fc52d401635d50c
gibo <sandro.burri@ti.ch>
2023-12-12 08:11:32 +0100

    check.R

---
+++ check.R
@@ -0,0 +1,50 @@
+
+
+# devtools (check, build, load, ...) ------------
+library(devtools)
+
+load_all(".")
+
+document()
+check_man()
+# check()
+check(args = "--no-tests")
+
+build()
+install(upgrade = "never")
+
+test()
+
+
+# Copia tar.gz nel progetto 00_copia_sorgenti_gib_packs -------------
+file.copy(
+  "../dcatapchr_0.0.0.0.tar.gz", to = "../00_copia_sorgenti_gib_packs/"
+)
+
+# Re-style R scripts
+styler::style_pkg()
+
+
+
+# usethis (test, ...) -----------------
+library(usethis)
+# create_package(path = ".")
+
+use_testthat()
+
+use_test("nome_funzione")
+
+
+# covr::package_coverage() ------------
+# Da eseguire in una nuova sessione R
+library(covr)
+cov <- package_coverage()
+cov
+# as.data.frame(cov)
+# report()
+report(cov, file = "dcatapchr-report.html")
+
+
+# pkgdown -----------------------------
+library(pkgdown)
+build_site()
