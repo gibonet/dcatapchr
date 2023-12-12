781c462f51b781dee1327162710186c1602613bf
gibo <sandro.burri@ti.ch>
2023-12-12 13:12:39 +0100

    complete dct_publisher with foaf tag...

--- R/dct_publisher.R
+++ R/dct_publisher.R
@@ -5,19 +5,28 @@
 #'
 #'
 #' @param publisher character string with a publisher (usually an URL)
+#' @param foaf_name name of the organization/publisher
 #'
 #'
 #' @export
-dct_publisher <- function(publisher = "https://www.ti.ch/ustat") {
+dct_publisher <- function(publisher = "https://www.ti.ch/ustat",
+                          foaf_name = "Ufficio di statistica del Cantone Ticino (Ustat)") {
 
   publisher <- add_quotes(publisher)
 
-  paste0(
-    "<dct:publisher rdf:resource=",
-    publisher,
-    "/>"
+  c(
+    "<dct:publisher>",
+    paste0("  <foaf:Organization rdf:about=", publisher, "/>"),
+    paste0("    <foaf:name>", foaf_name, "</foaf:name:>"),
+    "  </foaf:Organization>",
+    "</dct:publisher>"
   )
 }
 
 # <dct:publisher rdf:resource="https://www.ur.ch/departemente/58"/>
 
+# <dct:publisher>
+#   <foaf:Organization rdf:about="https://swisstopo">
+#     <foaf:name>Landesamt für Topographie Swisstopo</foaf:name:>
+#   </foaf:Organization>
+# </dct:publisher>
