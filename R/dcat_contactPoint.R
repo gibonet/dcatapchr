c1380b5a426f6e8d752bc59b6d53df5612713265
gibo <sandro.burri@ti.ch>
2023-12-12 11:42:32 +0100

    dcat_contactPoint() --> create a dcat:contactPoint property

---
+++ R/dcat_contactPoint.R
@@ -0,0 +1,34 @@
+
+
+
+#' Create a dcat:contactPoint property in DCAT-AP CH standard
+#'
+#' @param nodeID organization id (character string)
+#' @param email email address
+#'
+#' @export
+dcat_contactPoint <- function(nodeID = paste(sample(letters), collapse = ""),
+                              email = "dfe-ustat@ti.ch") {
+
+  stopifnot(length(nodeID) == 1L)
+  stopifnot(length(email) == 1L)
+
+  c(
+    "<dcat:contactPoint>",
+    paste0("  <vcard:Organization rdf:nodeID=", add_quotes(nodeID), ">"),
+    paste0("    <vcard:hasEmail rdf:resource=", add_quotes(paste0("mailto:", email)), "/>"),
+    paste0("    <vcard:fn>", email, "</vcard:fn>"),
+    "  </vcard:Organization>",
+    "</dcat:contactPoint>"
+  )
+}
+
+
+# <dcat:contactPoint>
+#   <vcard:Organization rdf:nodeID="Nb5448520ee3b44238b800a99424fbfb8">
+#     <vcard:hasEmail rdf:resource="mailto:awjf@vd.so.ch"/>
+#     <vcard:fn>awjf@vd.so.ch</vcard:fn>
+#   </vcard:Organization>
+# </dcat:contactPoint>
+
+
