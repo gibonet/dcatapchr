ae0ea25718833daa1c8c9c2331c0be76e7799f44
gibo <sandro.burri@ti.ch>
2023-12-12 13:44:03 +0100

    dct_rights()

---
+++ R/dct_rights.R
@@ -0,0 +1,24 @@
+
+
+#' Create a dct:rights property in DCAT-AP CH standard
+#'
+#' @param license one of the licenses allowed in opendata.swiss
+#'
+#' See also https://dcat-ap.ch/vocabulary/licenses/20210623.html#terms_open for a
+#' list of possible licenses.
+#'
+#' @export
+dct_rights <- function(license = c(
+  "NonCommercialAllowed-CommercialAllowed-ReferenceNotRequired",
+  "NonCommercialAllowed-CommercialAllowed-ReferenceRequired",
+  "NonCommercialAllowed-CommercialWithPermission-ReferenceNotRequired",
+  "NonCommercialAllowed-CommercialWithPermission-ReferenceRequired"
+)) {
+  license <- license[1]
+
+  paste0(
+    "<dct:rights>", license, "</dct:rights>"
+  )
+}
+
+# <dct:rights>NonCommercialAllowed-CommercialAllowed-ReferenceRequired</dct:rights>
