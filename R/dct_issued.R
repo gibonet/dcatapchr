9004ce08d065185424d56991d5bda46e8a92c865
gibo <sandro.burri@ti.ch>
2023-12-12 13:18:40 +0100

    dct_issued()

---
+++ R/dct_issued.R
@@ -0,0 +1,21 @@
+
+
+
+#' Create a dct:issued property in DCAT-AP CH standard
+#'
+#' @param date date or character vector in yyyy-mm-dd format
+#' @param hour_minutes_seconds hour, minutes and seconds (default: "00:00:00")
+#'
+#' @export
+dct_issued <- function(date = Sys.Date(),
+                       hour_minutes_seconds = "00:00:00") {
+  paste0(
+    "<dct:issued rdf:datatype=\"http://www.w3.org/2001/XMLSchema#dateTime\">",
+    date,
+    "T", hour_minutes_seconds,
+    "</dct:issued>"
+  )
+}
+
+# <dct:issued rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2023-03-01T00:00:00</dct:issued>
+
