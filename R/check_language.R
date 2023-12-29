1af3ad938d6c9f0cd4f1e35b6cd9a00d5725bda1
gibo <sandro.burri@ti.ch>
2023-12-29 10:16:18 +0100

    check_language() --> one of "de", "fr", "it" or "en"

---
+++ R/check_language.R
@@ -0,0 +1,14 @@
+
+
+
+#' Check language validity ("de", "fr", "it", "en")
+#'
+#' @param language one of "de", "fr", "it" or "en"
+#'
+check_language <- function(language) {
+  language <- language[1]
+
+  if (!(language %in% c("de", "fr", "it", "en"))) {
+    stop("Language must be one of de, fr, it or en")
+  }
+}
