e85c986a54022fa280a6da517eb769d99ec87aa9
gibo <sandro.burri@ti.ch>
2024-01-04 13:47:02 +0100

    add ... argument and some examples

--- R/dcat_catalog.R
+++ R/dcat_catalog.R
@@ -7,10 +7,33 @@
 #' @param catalog_endpoint URL/URI of the catalog endpoint
 #' @param datasets character vector with one or more dcat:dataset(s), see
 #'  [dcat_dataset()]
+#' @param ... any other property to be included to a dcat:Catalog
+#'
+#'
+#' @examples
+#' dcat_catalog()
+#' dcat_catalog(catalog_endpoint = "")
+#'
+#'
+#' # Add some properties (...)
+#' dcat_catalog(
+#'   title = dct_title("Catalogo Ustat", language = "it"),
+#'   description = dct_description("Descrizione catalogo", language = "it")
+#' )
 #'
 #' @export
 dcat_catalog <- function(catalog_endpoint = "https://swisstopo/catalog-endpoint.rdf",
-                         datasets = '<dcat:dataset rdf:resource="https://swisstopo/123"/>') {
+                         datasets = '<dcat:dataset rdf:resource="https://swisstopo/123"/>',
+                         ...) {
+
+  dots <- list(...)
+  if (length(dots) == 0L) {
+    dots <- NULL
+  } else {
+    # Convert to (character) vector
+    dots <- unlist(dots)
+  }
+
   # First part
   l1 <- '<?xml version="1.0" encoding="utf-8" ?>'
   l2 <- '<rdf:RDF '
@@ -35,11 +58,10 @@ dcat_catalog <- function(catalog_endpoint = "https://swisstopo/catalog-endpoint.
     )
   }
 
-
-
   c(
     f1,
     l4,
+    if (!is.null(dots)) paste0("    ", dots) else dots,
     paste0("    ", datasets),
     '  </dcat:Catalog>',
     '</rdf:RDF>'
