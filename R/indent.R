a178d233fc95e3404562038ffca5bd3524da058d
gibo <sandro.burri@ti.ch>
2024-01-09 14:59:08 +0100

    add indent() function (internal, not exported to the NAMESPACE)

---
+++ R/indent.R
@@ -0,0 +1,21 @@
+
+
+
+#' Indent a character vector by some spaces
+#'
+#'
+#' @param x a character vector
+#' @param nspaces number of desired spaces to indent (default: 2)
+#'
+#'
+indent <- function(x, nspaces = 2L) {
+  # first implementation
+  # s <- character(length = nspaces + 1L)
+  # s <- paste(s, collapse = " ")
+
+  # second implementation, with strrep
+  s <- strrep(" ", times = nspaces)
+
+  paste0(s, x)
+}
+
