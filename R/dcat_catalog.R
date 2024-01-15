ee9d2e8bad9fadab053da6c89921690b90d6a0b3
gibo <sandro.burri@ti.ch>
2024-01-15 09:20:34 +0100

    use rdf_namespaces() inside dcat_catalog()

--- R/dcat_catalog.R
+++ R/dcat_catalog.R
@@ -35,19 +35,7 @@ dcat_catalog <- function(catalog_endpoint = "https://swisstopo/catalog-endpoint.
   }
 
   # First part
-  l1 <- '<?xml version="1.0" encoding="utf-8" ?>'
-  l2 <- '<rdf:RDF '
-  l3 <- '  xmlns:foaf="http://xmlns.com/foaf/0.1/"'
-  l4 <- '  xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"'
-  l5 <- '  xmlns:hydra="http://www.w3.org/ns/hydra/core#"'
-  l6 <- '  xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"'
-  l7 <- '  xmlns:dcat="http://www.w3.org/ns/dcat#"'
-  l8 <- '  xmlns:dct="http://purl.org/dc/terms/"'
-  l9 <- '  xmlns:vcard="http://www.w3.org/2006/vcard/ns#"'
-  l10 <- '  xmlns:xsd="http://www.w3.org/2001/XMLSchema#"'
-  l11 <- '  xmlns:schema="http://schema.org/"'
-  l_end <- '>'
-  f1 <- c(l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l_end)
+  f1 <- rdf_namespaces()
 
   if (is.null(catalog_endpoint) || is.na(catalog_endpoint) ||
       catalog_endpoint == "") {
