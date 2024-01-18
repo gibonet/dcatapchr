c1c6d7fc93f8d17fb5dfac2d5a1d60c94e485254
gibo <sandro.burri@ti.ch>
2024-01-18 13:26:17 +0100

    add some @details --> properties that can be included

--- R/dcat_dataset.R
+++ R/dcat_dataset.R
@@ -1,6 +1,6 @@
 
 
-#' Create a DCAT-AP CH Catalog (dcat:dataset)
+#' Create a DCAT-AP CH Dataset (dcat:dataset)
 #'
 #' A dcat:dataset must have one or more dcat:distribution(s).
 #'
@@ -12,6 +12,27 @@
 #' @param distribution one or more distributions, see [dcat_distribution()]
 #' @param ... any other property to be included to a dcat:Dataset
 #'
+#'
+#' @details
+#' A dcat:Dataset class, created with dcat_dataset(),
+#' can/must have the following properties (M stands
+#' for mandatory; R for recommended):
+#' - dcat:contactPoint, M in V1 and V2: see [dcat_contactPoint()]
+#' - dct:description, M in V1 and V2: see [dct_description()]
+#' - dct:identifier, M in V1 and V2: see [dct_identifier()]
+#' - dct:publisher, M in V1 and V2: see [dct_publisher()]
+#' - dct:title, M in V1 and V2: see [dct_title()]
+#' - dcat:distribution, M in V1, R in V2: see [dcat_distribution()]
+#' - dcat:keyword, R in V2: see [dcat_keyword()]
+#' - dcat:landingPage, R in V2: see [dcat_landingPage()]
+#' - dct:issued, R in V2: see [dct_issued()]
+#' - dct:spatial, R in V2: see [dct_spatial()]
+#' - dct:temporal, R in V2: see [dct_temporal()]
+#' - dcat:theme, R in V2: see [dcat_theme()]
+#' - dct:modified, R in V2: see [dct_modified()]
+#'
+#'
+#' @return A character vector of length greater than one.
 #'
 #'
 #' @export
