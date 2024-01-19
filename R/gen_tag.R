f8f05665d057dc023e095b928ee5350aec1f27e2
gibo <sandro.burri@ti.ch>
2024-01-19 16:13:59 +0100

    functions gen_tag() and gen_tag() --> generate tags like <tag>...</tag> and <tag rdf:resource="..." />

---
+++ R/gen_tag.R
@@ -0,0 +1,39 @@
+
+
+
+#' Generate a tag like <tag>...</tag>
+#'
+#' @param tag character string with the desired tag
+#' @param x the content to be encapsulated inside the tag
+#'
+#'
+#' @export
+gen_tag <- function(tag = "dct:indentifier", x = "...") {
+  stopifnot(length(tag) == 1L)
+
+  paste0(
+    "<", tag, ">",
+    x,
+    "</", tag, ">"
+  )
+}
+
+
+#' Generate a tag like <tag rdf:resource="..." />
+#'
+#'
+#' @param tag character string with the desired tag
+#' @param x the content to be encapsulated inside the tag. Note that this
+#'     content is enclosed in quotation marks (by the function)
+#' @param rdf_resource character string that
+#'
+#' @export
+gen_tag2 <- function(tag = "dcat:accessURL", x = "https://example.com", rdf_resource = "rdf:resource") {
+  stopifnot(length(tag) == 1L)
+
+  paste0(
+    "<", tag, " ", rdf_resource, "=", add_quotes(x), " />"
+  )
+}
+
+# <dcat:accessURL rdf:resource=\"https://www3.ti.ch/DFE/DR/USTAT/allegati/cubo/cubi_POL_01_csv.zip\" />
