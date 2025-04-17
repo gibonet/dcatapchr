e08bc10ade625752ae1683a51bbf4592e4a903e2
gibo <sandro.burri@ti.ch>
2025-04-17 11:09:11 +0200

    test dct_publisher2()

--- tests/testthat/test-dct_publisher.R
+++ tests/testthat/test-dct_publisher.R
@@ -12,3 +12,19 @@ test_that("dct_publisher returns an error if one of the arguments have a length 
   expect_error(dct_publisher(foaf_name = NULL))
 })
 
+test_that("dct_publisher2 allows more languages", {
+  res <- dct_publisher2(
+    publisher = "https://www.ti.ch/ustat",
+    foaf_name = c(
+      "Ufficio di statistica del Cantone Ticino (Ustat)",
+      "Office de statistique du Canton Tessin (Ustat)"
+    ),
+    language = c("it", "fr")
+  )
+
+  expect_type(res, "character")
+  expect_gt(length(res), 1L)
+
+  rm(res)
+})
+
