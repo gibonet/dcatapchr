5a13d3b08324b88967b6aad5561125b1cfbb3968
gibo <sandro.burri@ti.ch>
2025-06-12 14:40:36 +0200

    dct_issued() example

--- R/dct_issued.R
+++ R/dct_issued.R
@@ -6,6 +6,9 @@
 #' @param date date or character vector in yyyy-mm-dd format
 #' @param hour_minutes_seconds hour, minutes and seconds (default: "00:00:00")
 #'
+#' @examples
+#' dct_issued(date = "2000-12-31", hour_minutes_seconds = "10:00:00")
+#'
 #' @export
 dct_issued <- function(date = Sys.Date(),
                        hour_minutes_seconds = "00:00:00") {
