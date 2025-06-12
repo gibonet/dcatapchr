a96c21f49db27e4d0bc3dacbc71be3e1c4c277c7
gibo <sandro.burri@ti.ch>
2025-06-12 10:07:02 +0200

    add examples of add_quotes()

--- R/add_quotes.R
+++ R/add_quotes.R
@@ -5,6 +5,8 @@
 #'
 #' @param x a character string (or vector)
 #'
+#' @example inst/examples/ex-add_quotes.R
+#'
 #' @export
 add_quotes <- function(x) {
   paste0("\"", x, "\"")
---
+++ inst/examples/ex-add_quotes.R
@@ -0,0 +1,5 @@
+# Add quotes around a character string
+add_quotes("A character string")
+
+# Add quotes around a vector of strings
+add_quotes(c("a", "b"))
