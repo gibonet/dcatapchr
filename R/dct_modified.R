9504a93c0daec11101dcdca0c1cc86061f5f378a
gibo <sandro.burri@ti.ch>
2025-06-12 14:45:41 +0200

    dct_modified() example

--- R/dct_modified.R
+++ R/dct_modified.R
@@ -9,6 +9,9 @@
 #' @param date date or character vector in yyyy-mm-dd format
 #' @param hour_minutes_seconds hour, minutes and seconds (default: "00:00:00")
 #'
+#' @examples
+#' dct_modified(date = "2001-12-31", hour_minutes_seconds = "10:00:00")
+#'
 #' @export
 dct_modified <- function(date = Sys.Date(),
                        hour_minutes_seconds = "00:00:00") {
